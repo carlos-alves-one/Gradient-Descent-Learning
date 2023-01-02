463df3c6886b26417c92679b0890bacd6708b378
Carlos Alves <carlos.alves.one@gmail.com>
2023-01-02 14:08:46 +0000

    Define the number of epochs (iterations) and the learning rate

--- code.m
+++ code.m
@@ -2,3 +2,8 @@
 % Author....: Carlos Manuel de Oliveira Alves
 % Student...: cdeol003
 % Created...: 01/01/2023
+
+% Define the number of epochs (iterations) and the learning rate
+numEpochs = 100;
+learningRate = 0.1;
+
